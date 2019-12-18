9655c05d6dfa55027d8192f11626d4e76e035eca
peter <twinkle-pirate@hotmail.com>
2019-12-18 13:54:33 +0800

    add screen now

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -18,22 +18,7 @@ C_DEPS :=
 SUBDIRS := \
 Core/Src \
 Core/Startup \
-Drivers/BSP/Adafruit_Shield \
-Drivers/BSP/Components/cs43l22 \
-Drivers/BSP/Components/exc7200 \
-Drivers/BSP/Components/ft6x06 \
-Drivers/BSP/Components/ili9325 \
 Drivers/BSP/Components/ili9341 \
-Drivers/BSP/Components/l3gd20 \
-Drivers/BSP/Components/lis302dl \
-Drivers/BSP/Components/lis3dsh \
-Drivers/BSP/Components/ls016b8uy \
-Drivers/BSP/Components/lsm303dlhc \
-Drivers/BSP/Components/mfxstm32l152 \
-Drivers/BSP/Components/otm8009a \
-Drivers/BSP/Components/ov2640 \
-Drivers/BSP/Components/s5k5cag \
-Drivers/BSP/Components/st7735 \
 Drivers/BSP/Components/st7789h2 \
 Drivers/BSP/Components/stmpe1600 \
 Drivers/BSP/Components/stmpe811 \
